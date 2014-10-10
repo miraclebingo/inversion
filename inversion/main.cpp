bba3aa5caeb6bee760f3add3460b37241609528a
Zhongzhou Cai <zhongzhoucai@ZhongzhoudeMacBook-Pro.local>
2014-10-10 02:30:19 -0400

    Initial Commit

---
+++ inversion/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  inversion
+//
+//  Created by Zhongzhou Cai on 10/10/14.
+//  Copyright (c) 2014 Zhongzhou Cai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
